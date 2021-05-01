79ef63064ab18af6d23aefde568089dd97b5510d
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-05-01 20:30:25 +0300

    Better templates: 'has_begin_end' and 'has_const_iterator'

--- headers/alog/tools.h
+++ headers/alog/tools.h
@@ -366,51 +366,38 @@ private:
 };
 #pragma pack(pop)
 
-struct sfinae_base
+template<typename T>
+struct has_const_iterator
 {
-    using yes = char;
-    using no  = yes[2];
+private:
+    typedef char                      yes;
+    typedef struct { char array[2]; } no;
+
+    template<typename C> static yes test(typename C::const_iterator*);
+    template<typename C> static no  test(...);
+public:
+    static const bool value = sizeof(test<T>(0)) == sizeof(yes);
+    typedef T type;
 };
 
 template <typename T>
-struct has_const_iterator : private sfinae_base
+struct has_begin_end
 {
-private:
-    template <typename C> static yes & test(typename C::const_iterator*);
-    template <typename C> static no  & test(...);
-public:
-    static const bool value = sizeof(test<T>(nullptr)) == sizeof(yes);
-    using type =  T;
+    template<typename C> static char (&f(typename std::enable_if<
+      std::is_same<decltype(static_cast<typename C::const_iterator (C::*)() const>(&C::begin)),
+      typename C::const_iterator(C::*)() const>::value, void>::type*))[1];
 
-    void dummy(); //for GCC to supress -Wctor-dtor-privacy
-};
+    template<typename C> static char (&f(...))[2];
 
-template <typename T>
-struct has_begin_end : private sfinae_base
-{
-private:
-    template <typename C>
-    static yes & f(typename std::enable_if<
-                   std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::begin)),
-                   typename C::const_iterator(C::*)() const>::value>::type*);
+    template<typename C> static char (&g(typename std::enable_if<
+      std::is_same<decltype(static_cast<typename C::const_iterator (C::*)() const>(&C::end)),
+      typename C::const_iterator(C::*)() const>::value, void>::type*))[1];
 
-    template <typename C>
-    static no & f(...);
+    template<typename C> static char (&g(...))[2];
 
-    template <typename C>
-    static yes & g(typename std::enable_if<
-                   std::is_same<decltype(static_cast<typename C::const_iterator(C::*)() const>(&C::end)),
-                   typename C::const_iterator(C::*)() const>::value, void>::type*);
-
-    template <typename C>
-    static no & g(...);
-
-public:
-    static bool const beg_value = sizeof(f<T>(nullptr)) == sizeof(yes);
-    static bool const end_value = sizeof(g<T>(nullptr)) == sizeof(yes);
+    static bool const beg_value = sizeof(f<T>(0)) == 1;
+    static bool const end_value = sizeof(g<T>(0)) == 1;
     static bool const value = beg_value && end_value;
-
-    void dummy(); //for GCC to supress -Wctor-dtor-privacy
 };
 
 template <typename T>
