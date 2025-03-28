7db4f63c4ac7f0ddd84bad9e2c81b199aaab2518
Ihor Drachuk <ihor-drachuk-libs@pm.me>
2025-03-28 20:14:34 +0200

    Added date & time and configuration flags to Default formatter

--- src/formatters/default.cpp
+++ src/formatters/default.cpp
@@ -7,6 +7,8 @@
 namespace ALog {
 namespace Formatters {
 
+namespace {
+
 static const char* const severitiesMap[] = {
     "Verb",
     "Debug",
@@ -16,27 +18,53 @@ static const char* const severitiesMap[] = {
     "Crit!"
 };
 
-struct Default::impl_t {
+template<typename T, typename Timepoint>
+unsigned long long unsigned_duration_cast(const Timepoint& tp)
+{
+    return static_cast<unsigned long long>(std::chrono::duration_cast<T>(tp).count());
+}
+
+} // namespace
+
+struct Default::impl_t
+{
     mutable ALog::Buffer cache;
+    Internal::Flags<Flag> flags;
 };
 
-Default::Default()
+Default::Default(Internal::Flags<Flag> flags)
 {
     createImpl();
+    impl().flags = flags;
 }
 
 Default::~Default() = default;
 
 Buffer Default::format(const Record& record) const
 {
-    using namespace std::chrono;
-
-    auto currentTime = (record.steadyTp - record.startTp);
-    unsigned long long secs = duration_cast<seconds>(currentTime).count();
-    uint16_t msecs = static_cast<uint16_t>(duration_cast<milliseconds>(currentTime).count() - secs * 1000);
-
     ALog::I::LongSSO<> result(impl().cache);
-    result.appendFmtString("[%3llu.%03hu] T#%-2d ", secs, msecs, record.threadNum);
+
+    if (impl().flags & Flag::DateTime) {
+        std::time_t timeT = std::chrono::system_clock::to_time_t(record.systemTp);
+        std::tm tmTime;
+#ifdef _WIN32
+        localtime_s(&tmTime, &timeT);
+#else
+        localtime_r(&timeT, &tmTime);
+#endif // _WIN32
+        result.appendFmtString("[%02d%02d%02d_%02d%02d%02d] ",
+                               tmTime.tm_year % 100, tmTime.tm_mon + 1, tmTime.tm_mday,
+                               tmTime.tm_hour, tmTime.tm_min, tmTime.tm_sec);
+    }
+
+    if (impl().flags & Flag::LocalTimestamp) {
+        const auto currentTime = (record.steadyTp - record.startTp);
+        const unsigned long long secs = unsigned_duration_cast<std::chrono::seconds>(currentTime);
+        const uint16_t msecs = static_cast<uint16_t>(unsigned_duration_cast<std::chrono::milliseconds>(currentTime) - secs * 1000); // NOLINT(hicpp-use-auto)
+        result.appendFmtString("[%3llu.%03hu] ", secs, msecs);
+    }
+
+    result.appendFmtString("T#%-2d ", record.threadNum);
 
     if (record.threadTitle)
         result.appendFmtString("(%s) ", record.threadTitle);
